5c26d51f922691ee2684373d862d103285f73049
Leonardo Rodrigues <leonardorodrigues@Air-de-Leonardo.local>
2022-08-09 22:39:57 -0300

    feat: atualizado o serviço de gerenciamento de token, utilizando os cookies com a lib nookies

--- frontend/src/pages/auth-page-ssr.js
+++ frontend/src/pages/auth-page-ssr.js
@@ -1,14 +1,23 @@
+import nookies from "nookies";
+import { tokenService } from "../services/auth/tokenService";
+
+export async function getServerSideProps(ctx) {
+  const cookies = nookies.get(ctx);
+  console.log("cookies", cookies);
+  return {
+    props: {
+      token: tokenService.get(ctx),
+    },
+  };
+}
+
 function AuthPageSSR(props) {
   return (
     <div>
-      <h1>
-        Auth Page Server Side Render
-      </h1>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre> 
+      <h1>Auth Page Server Side Render</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
-  )
+  );
 }
 
 export default AuthPageSSR;
--- frontend/src/services/auth/tokenService.js
+++ frontend/src/services/auth/tokenService.js
@@ -1,16 +1,31 @@
+import nookies from "nookies";
+
 const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
 
+const ONE_SECOND = 1;
+const ONE_MINUT = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUT * 60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_YEAR = ONE_DAY * 365;
+
 export const tokenService = {
-  save: (accessToken) => {
-    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+  save: (accessToken, ctx = null) => {
+    // localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
     // sessionStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+    nookies.set(ctx, ACCESS_TOKEN_KEY, accessToken, {
+      maxAge: ONE_YEAR,
+      path: "/",
+    });
   },
-  get: () => {
-    return localStorage.getItem(ACCESS_TOKEN_KEY);
+  get: (ctx = null) => {
+    // return localStorage.getItem(ACCESS_TOKEN_KEY);
     // return sessionStorage.getItem(ACCESS_TOKEN_KEY);
+    const cookies = nookies.get(ctx);
+    return cookies[ACCESS_TOKEN_KEY] || "";
   },
-  delete: () => {
-    localStorage.removeItem(ACCESS_TOKEN_KEY);
+  delete: (ctx = null) => {
+    // localStorage.removeItem(ACCESS_TOKEN_KEY);
     // sessionStorage.removeItem(ACCESS_TOKEN_KEY);
+    nookies.destroy(ctx, ACCESS_TOKEN_KEY);
   },
 };
