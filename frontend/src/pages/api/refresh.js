13392450faeaec6cc9e545ac6a1a474423a609cf
Leonardo Rodrigues <leonardorodrigues@MacBook-Air-de-Leonardo.local>
2022-08-11 15:48:47 -0300

    feat: implementado rota e controller que atualizam o refresh token do usuario

--- frontend/src/pages/api/refresh.js
+++ frontend/src/pages/api/refresh.js
@@ -34,11 +34,52 @@ const controllers = {
       cookies: nookies.get(ctx),
     });
   },
+  async regerenareToken(req, res) {
+    const ctx = { req, res };
+
+    const cookies = nookies.get(ctx);
+    const refresh_token = cookies[REFRESH_TOKEN_NAME];
+
+    const url = "http://localhost:4000/api/refresh";
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refresh_token }),
+    };
+
+    const refreshedResponse = await fetch(url, options)
+      .then(async (res) => {
+        const resposta = await res.json();
+        return resposta;
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    try {
+      nookies.set(
+        ctx,
+        REFRESH_TOKEN_NAME,
+        refreshedResponse.data.refresh_token,
+        {
+          httpOnly: true,
+          sameSite: "lax",
+        }
+      );
+
+      res.json({ refreshedResponse });
+    } catch (err) {
+      res.status(401).json({ msg: "unauthorized", status: 401 });
+    }
+  },
 };
 
 const controllerBy = {
   POST: controllers.storeRefreshToken,
-  GET: controllers.getInformation,
+  //   GET: controllers.getInformation,
+  GET: controllers.regerenareToken,
 };
 
 export default function handler(request, response) {
